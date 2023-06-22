e5bdcd46236b7e1109206df823a1e87a10fc1804
hiiragi390 <hiiragi390@gmail.com>
2023-06-22 12:47:45 +0900

    データ送信消えてたので復活

--- src/AddData.js
+++ src/AddData.js
@@ -7,22 +7,22 @@ let Q_id;
 
 function Send(Q_list){
     console.log(rect,Q_list);
-    // try{  
-    //     for(Q_id of Q_list){
-    //         console.log(Q_id);
-    //         setDoc(doc(db,"Q", "result", Q_id, key),{          
-    //             radius:rect
-    //         }
-    //         );
-    //         setDoc(doc(db,"documents_list",Q_id),{
-    //             document: key
-    //         })
-    //         console.log("completed"); 
-    //     }  
-    // }
-    // catch(e){
-    //     console.log(e);
-    // } 
+    try{  
+        for(Q_id of Q_list){
+            console.log(Q_id);
+            setDoc(doc(db,"Q", "result", Q_id, key),{          
+                radius:rect
+            }
+            );
+            setDoc(doc(db,"documents_list",Q_id),{
+                document: key
+            })
+            console.log("completed"); 
+        }  
+    }
+    catch(e){
+        console.log(e);
+    } 
 }
 
 export default Send;
