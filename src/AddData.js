1b3095003bc35c9607f6356dc9a5e576d88a6eac
hiiragi390 <hiiragi390@gmail.com>
2023-07-13 14:03:51 +0900

    test

--- src/AddData.js
+++ src/AddData.js
@@ -30,7 +30,7 @@ function Send(Q_list){
         let i=0;
         for(let k of Radio_A_list){
             let v = k[1];
-            setDoc(doc(db,"Q", "Radio_result", Q_id[i], key),{          
+            setDoc(doc(db,"Q", "Radio_result", Q_id, key),{          
                 anser:v
             }
             );
