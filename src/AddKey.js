291a47a6268696ccf677484fdee50544d1c06bda
hiiragi390 <hiiragi390@gmail.com>
2023-07-13 13:17:52 +0900

    cokkie-2

--- src/AddKey.js
+++ src/AddKey.js
@@ -1,7 +1,21 @@
 import key from "./Key";
 
+
 export function AddKey(){
-    if(document.cookie==null){
+    let G_cookie = true;
+    let cookies = document.cookie;
+    let cookieItem = cookies.split(";");
+    let cookieValue = "";
+
+    for(let i=0;i<cookieItem.length;i++){
+        let el = cookieItem[i].split("=");
+        if(el[0].trim() == "key"){
+            G_cookie = false;
+            key = el[1];
+        }
+    }
+
+    if(G_cookie){
         document.cookie = "key=" + key;
     }
     console.log(document.cookie);
