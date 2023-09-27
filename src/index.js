abe97f8b7673ba129814b693311096bd217edc9b
hiiragi390 <hiiragi390@gmail.com>
2023-09-27 19:51:50 +0900

    testpage

--- src/index.js
+++ src/index.js
@@ -1,16 +1,30 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import Test1 from "./Test1";
+import Test2 from "./Test2";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById("root")
-);
+const container = document.getElementById("root");
+if(container){
+  const root = ReactDOM.createRoot(container);
+  root.render(
+    <React.StrictMode >
+      <BrowserRouter basename={process.env.PUBLIC_URL}>
+        <Routes>
+          <Route path="/" element={<Test1 />}></Route>
+          <Route path="/q-test/even" element={<Test2 />}></Route>
+        </Routes>
+        </BrowserRouter>
+    </React.StrictMode>
+  );
+}
+else{
+  console.log("tst");
+}
+
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
