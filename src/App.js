dc26ebb45a58895e97d591a8b0aca48f06dcecec
hiiragi390 <hiiragi390@gmail.com>
2023-09-27 19:33:00 +0900

    page設定

--- src/App.js
+++ src/App.js
@@ -35,7 +35,7 @@ const App = () => {
     <div>
     <BrowserRouter basename={process.env.PUBLIC_URL}>
       <Routes>
-        <Route path="/q-test/odd" element={<Test1 />}></Route>
+        <Route path="/q-test/" element={<Test1 />}></Route>
         <Route path="/q-test/even" element={<Test2 />}></Route>
       </Routes>
       </BrowserRouter>
